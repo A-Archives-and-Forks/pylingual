1c3992079d9c43db8c308b75298e37a1d7f203ed
Joel Flores <jjf190004@utdallas.edu>
2025-12-11 12:59:53 -0600

    bytecode patch update for 3.14

--- pylingual/editable_bytecode/bytecode_patches/replace_firstlno.py
+++ pylingual/editable_bytecode/bytecode_patches/replace_firstlno.py
@@ -4,7 +4,7 @@ import itertools
 
 
 def replace_firstlno(bytecode: EditableBytecode):
-    to_replace = next((load_const for load_const, store_name in itertools.pairwise(bytecode.instructions) if load_const.opname == "LOAD_CONST" and store_name.opname == "STORE_NAME" and store_name.argval == "__firstlineno__"), None)
+    to_replace = next((load_const for load_const, store_name in itertools.pairwise(bytecode.instructions) if load_const.opname in ["LOAD_CONST", "LOAD_SMALL_INT"] and store_name.opname == "STORE_NAME" and store_name.argval == "__firstlineno__"), None)
     if to_replace is not None:
         to_replace.argval = 0
         to_replace.argrepr = "0"
