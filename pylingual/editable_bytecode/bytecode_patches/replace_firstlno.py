34fe0fa356d7b2db4d10ed52beb006b43e07f943
Josh Wiedemeier <jdw170000@utdallas.edu>
2025-12-17 10:34:23 -0600

    adjust firstlno patch for 314 load small int

--- pylingual/editable_bytecode/bytecode_patches/replace_firstlno.py
+++ pylingual/editable_bytecode/bytecode_patches/replace_firstlno.py
@@ -4,7 +4,12 @@ import itertools
 
 
 def replace_firstlno(bytecode: EditableBytecode):
-    to_replace = next((load_const for load_const, store_name in itertools.pairwise(bytecode.instructions) if load_const.opname in ["LOAD_CONST", "LOAD_SMALL_INT"] and store_name.opname == "STORE_NAME" and store_name.argval == "__firstlineno__"), None)
+    to_replace = next(
+        (load_const for load_const, store_name in itertools.pairwise(bytecode.instructions) if load_const.opname in ["LOAD_CONST", "LOAD_SMALL_INT"] and store_name.opname == "STORE_NAME" and store_name.argval == "__firstlineno__"), None
+    )
     if to_replace is not None:
         to_replace.argval = 0
         to_replace.argrepr = "0"
+
+        if bytecode.version >= (3, 14):
+            to_replace.opname = "LOAD_SMALL_INT"
